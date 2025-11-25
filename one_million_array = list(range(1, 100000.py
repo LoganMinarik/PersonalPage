a0a512267f9e9dcfbac85d2ebe94089056f54df1
LoganMinarik <168377908+LoganMinarik@users.noreply.github.com>
2025-11-24 20:39:40 -0500

    Create one_million_array = list(range(1, 100000.py

---
+++ one_million_array = list(range(1, 100000.py
@@ -0,0 +1,10 @@
+one_million_array = list(range(1, 1000001))
+
+# To prove the code works, we can print the first 10 numbers and the total count.
+# We will not print the entire array because it's too large.
+
+print("The first 10 numbers in the array are:")
+print(one_million_array[:1000000])
+
+print("\nThe total number of items in the array is:")
+print(len(one_million_array))
